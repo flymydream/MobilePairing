2f9ea8017711c640dab10d4faa01ec7da6a47351
flymydream <1271318591@qq.com>
2022-03-10 20:09:02 +0800

    health

---
+++ JFWechatMoments/Common/HealthkitViewController.h
@@ -0,0 +1,17 @@
+//
+//  HealthkitViewController.h
+//  JFWechatMoments
+//
+//  Created by hero on 2022/3/10.
+//  Copyright © 2022 fanzhiying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HealthkitViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
